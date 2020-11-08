1730acdc2170954f8ad8c2596ec48dc68c188e96
RachelSmith500 <rachelsmith9721@gmail.com>
2020-11-08 15:37:43 -0600

    get functions made for getting details and genre

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -4,8 +4,34 @@ import { connect } from 'react-redux';
 
 class Details extends Component{
 
+    state ={
+        details: [],
+        genres: []
+    }
     
-    // props.history.push('/home')
+    componentDidMount = () => {
+        this.getDetails();
+        this.getGenres();
+        this.setState({
+            details: this.props.reduxState.details ? this.props.reduxState.details: [],
+            genre: this.props.reduxState.genres ? this.props.reduxState.genre : []
+        })
+    }
+
+    getDetails = () => {
+        this.props.dispatch({type: 'GET_DETAILS', payload:this.props.match.params.id})
+    }
+    getGenres = () => {
+        this.props.dispatch({type: 'GET_GENRES'})
+    }
+    
+    getGenreIndex = () => {
+        for(let genre of genres){
+            if(genre.id === genreTag.genres_id){
+                return genres,indexOf(genre);
+            }
+        }
+    }
     
     render(){
         const movies = this.props.reduxState.movies ? this.props.reduxState.movies: [];
